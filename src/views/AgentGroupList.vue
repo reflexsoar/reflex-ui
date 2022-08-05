6a4d93b612f63b57bfd82721701f86e8a6a88fef
Brian Carroll <bcarroll@zeroonesecurity.com>
2022-08-04 22:45:11 -0400

    organization badge fixes

--- src/views/AgentGroupList.vue
+++ src/views/AgentGroupList.vue
@@ -24,7 +24,7 @@
       </template>
       <template #organization="{item}">
                 <td>
-                  <CButton class="tag" size="lg" color="secondary">{{mapOrgToName(item.organization)}}</CButton>
+                  <OrganizationBadge :uuid="item.organization"/>
                 </td>
               </template>
       <template #actions="{item}">
@@ -120,8 +120,12 @@
 
 <script>
 import {mapState} from "vuex";
+import OrganizationBadge from './OrganizationBadge'
 export default {
     name: 'AgentGroups',
+    components: {
+      OrganizationBadge
+    },
     props: {
     items: Array,
     fields: {
