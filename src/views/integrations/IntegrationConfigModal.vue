1fe4064f6e3ddcc47e71bb1ea24bd6e223e40fc0
Brian Carroll <brian@hasecuritysolutions.com>
2023-08-22 03:25:53 -0400

    Support for proper field labels and multi select

--- src/views/integrations/IntegrationConfigModal.vue
+++ src/views/integrations/IntegrationConfigModal.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
+  <div><link
+      rel="stylesheet"
+      href="https://unpkg.com/vue-multiselect@2.1.0/dist/vue-multiselect.min.css"
+    />
     <form id="createIntegrationConfig" @submit.prevent="submitForm">
       <CModal :title="title" size="xl" :show.sync="modalStatus">
         <CAlert v-if="error" :show="error" color="danger" fade>
@@ -46,7 +49,7 @@
                 <CInput
                   v-model="configuration.global_settings[data]"
                   v-if="field.type == 'str'"
-                  :label="data"
+                  :label="field.label"
                   :description="field.description"
                   :placeholder="field.default"
                   v-bind:required="isRequired()"
@@ -65,7 +68,7 @@
                 <CTextarea
                   v-model="configuration.global_settings[data]"
                   v-if="field.type == 'text'"
-                  :label="data"
+                  :label="field.label"
                   :description="field.description"
                   :placeholder="field.default"
                   v-bind:required="field.required"
@@ -73,7 +76,7 @@
                 <CInput
                   v-model.number="configuration.global_settings[data]"
                   v-if="field.type == 'int'"
-                  :label="data"
+                  :label="field.label"
                   :description="field.description"
                   :placeholder="field.default"
                   v-bind:required="field.required"
@@ -81,7 +84,7 @@
                 <CSelect
                   v-model="configuration.global_settings[data]"
                   v-if="field.type == 'str-select'"
-                  :label="data"
+                  :label="field.label"
                   :options="field.options"
                   :description="field.description"
                   :placeholder="field.default"
@@ -128,11 +131,12 @@
                   <h4>Action Specific Settings</h4></span
                 >
                 <CRow v-for="(field, data) in action.configuration">
-                  <CCol>
+                  <CCol v-if="!field.conditions ||
+                  (field.conditions && field.conditions && field.conditions.every((c) => evaluateFieldValue(configuration.actions[action.name][c.field], c.operator, c.value)))">
                     <CInput
                       v-model="configuration.actions[action.name][data]"
                       v-if="field.type == 'str'"
-                      :label="data"
+                      :label="field.label"
                       :description="field.description"
                       :placeholder="field.default"
                       v-bind:required="
@@ -142,7 +146,7 @@
                     <CTextarea
                       v-model="configuration.actions[action.name][data]"
                       v-if="field.type == 'text'"
-                      :label="data"
+                      :label="field.label"
                       :description="field.description"
                       :placeholder="field.default"
                       v-bind:required="
@@ -152,7 +156,7 @@
                     <CInput
                       v-model.number="configuration.actions[action.name][data]"
                       v-if="field.type == 'int'"
-                      :label="data"
+                      :label="field.label"
                       :description="field.description"
                       :placeholder="field.default"
                       v-bind:required="
@@ -162,7 +166,7 @@
                     <CSelect
                       :value.sync="configuration.actions[action.name][data]"
                       v-if="field.type == 'str-select'"
-                      :label="data"
+                      :label="field.label"
                       :options="field.options"
                       :description="field.description"
                       :placeholder="field.default"
@@ -171,7 +175,7 @@
                       "
                     />
                     <div v-if="field.type == 'bool'">
-                      <label>{{ data }}</label
+                      <label>{{ field.label }}</label
                       ><br />
                       <CSwitch
                         :checked.sync="configuration.actions[action.name][data]"
@@ -180,6 +184,22 @@
                         color="success"
                       /><br />
                       <small>{{ field.description }}</small>
+                    </div>
+                    <div v-if="field.type == 'str-multiple'">
+                      <label style="text-transform: capitalize">{{ field.label }}</label
+                      ><br />
+                      <multiselect
+                        v-model="configuration.actions[action.name][data]"
+                        @tag="addMultiOption(action.name, data, $event)"
+                        @remove="removeMultiOption(action.name, data, $event)"
+                        :options="getSelectOptions(field, field.default_options_from)"
+                        :multiple="true"
+                        :close-on-select="false"
+                        :placeholder="field.description"
+                        :taggable="true"
+                      >
+                      </multiselect>
+                      <small class="text-muted">{{ field.description }}</small><br><br>
                     </div>
                   </CCol>
                 </CRow>
@@ -351,6 +371,66 @@ export default {
     },
     toggleSecrets() {
       this.show_secrets = !this.show_secrets;
+    },
+    evaluateFieldValue(target, operator, value) {
+      if (operator == "eq") {
+        return target == value;
+      }
+      if (operator == "ne") {
+        return target != value;
+      }
+      if (operator == "gt") {
+        return target > value;
+      }
+      if (operator == "lt") {
+        return target < value;
+      }
+      if (operator == "gte") {
+        return target >= value;
+      }
+      if (operator == "lte") {
+        return target <= value;
+      }
+      if (operator == "contains") {
+        return target.includes(value);
+      }
+      if (operator == "startswith") {
+        return target.startsWith(value);
+      }
+      if (operator == "endswith") {
+        return target.endsWith(value);
+      }
+      if (operator == "regex") {
+        return target.match(value);
+      }
+    },
+    getSelectOptions(field_config, field) {
+      let values = [];
+      if (field == "intel_list") {
+        values = this.$store.getters.intel
+      }
+      return [...new Set(values)];
+    },
+    addMultiOption(action, field, $event) {
+      if (!this.configuration.actions[action][field]) {
+        this.$set(this.configuration.actions[action], field, []);
+      }
+      if (this.configuration.actions[action][field].includes($event)) {
+        return;
+      } else {
+        // If the $event has a value property, use that, otherwise use the $event
+        if ($event.value) {
+          this.configuration.actions[action][field].push($event.value);
+        } else {
+            this.configuration.actions[action][field].push($event);
+        }
+      }
+    },
+    removeMultiOption(action, field, $event) {
+      // Update in a manor that is reactive per Vue using $set
+      let tags = this.configuration.actions[action][field];
+      tags = tags.filter((tag) => tag != $event);
+      this.$set(this.configuration.actions[action], field, tags);
     },
     dismiss() {
       this.error = false;
