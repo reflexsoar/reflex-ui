683430c1a56717190111271851a3ac5e269e41ec
Brian Carroll <brian@hasecuritysolutions.com>
2023-11-16 11:22:42 -0500

    benchmark scoring updates

--- src/views/benchmark/BenchmarkScore.vue
+++ src/views/benchmark/BenchmarkScore.vue
@@ -1,19 +1,20 @@
 <template>
     <div>
         <CRow>
-            <CCol class="text-center">
-                <CBadge color="secondary" class="tag">{{ score.total }}</CBadge>
+            <CCol class="text-center" >
+                <CBadge color="info" class="tag" v-c-tooltip="{'content': 'Total Assets'}">{{ score.total }}</CBadge>
             </CCol>
-
-            <CCol class="text-center">
-                <CBadge color="success" class="tag">{{ score.passed }}</CBadge>
+            <CCol class="text-center" >
+                <CBadge color="success" class="tag" v-c-tooltip="{'content': 'Passed'}">{{ score.passed }}</CBadge>
             </CCol>
-            <CCol class="text-center">    
-                <CBadge color="danger" class="tag">{{ score.failed }}</CBadge>
+            <CCol class="text-center" >    
+                <CBadge color="danger" class="tag" v-c-tooltip="{'content': 'Failed'}">{{ score.failed }}</CBadge>
             </CCol>
-            <CCol class="text-center">
-                <CBadge color="warning" class="tag">{{ score.error }}</CBadge>
-
+            <CCol class="text-center" >
+                <CBadge color="warning" class="tag" v-c-tooltip="{'content': 'Error'}">{{ score.error }}</CBadge>
+            </CCol>
+            <CCol class="text-center" >
+                <CBadge color="secondary" class="tag" v-c-tooltip="{'content': 'Skipped'}">{{ score.skipped }}</CBadge>
             </CCol>
         </CRow>
     </div>
