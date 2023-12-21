9070bc3e7eafbbfd30394dd62fe28334d4b47eec
Brian Carroll <brian@hasecuritysolutions.com>
2023-12-21 15:52:22 -0500

    login screen changes

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,73 +1,107 @@
 <template>
-  <CContainer class="d-flex content-center min-vh-100">
-    <CRow>
-      <CCol>
-        <CAlert v-if="authStatus() == false"
-              closeButton
-              color="danger"
-              fade
-            >
-              Failed to login. Invalid username or password.
-            </CAlert>
-        <CAlert v-if="sso_disabled"
-            closeButton
-            color="danger"
-            fade
-          >
-            Unable to login with SSO.  Contact your ReflexSOAR Administrator.
-          </CAlert>
-        <CCardGroup>
-          <CCard color="light">
-            <CCardBody>
-              
-              <CForm @submit.prevent="login">
-                <h1>Login</h1>
-                <p class="text-muted">Sign In to your account</p>
-                <CInput
-                  placeholder="Email"
-                  autocomplete="username email"
-                  required
-                  v-model="email"
-                >
-                  <template #prepend-content><CIcon name="cil-user"/></template>
-                </CInput>
-                <CInput
-                  placeholder="Password"
-                  type="password"
-                  autocomplete="current-password"
-                  required
-                  v-model="password"
-                >
-                  <template #prepend-content><CIcon name="cil-lock-locked"/></template>
-                </CInput>
-                <CRow>
-                  <CCol col="6" class="text-left">
-                    <CButton color="primary" class="px-4" type="submit">Login</CButton>&nbsp;
-                    <CButton color="secondary" @click="loginWithSSO">Login with SSO</CButton>
-                  </CCol>
-                  <CCol col="6" class="text-right">
-                    <CButton color="secondary" to="/forgot_password">Forgot password?</CButton>
-                  </CCol>
-                </CRow>
-              </CForm>
-            </CCardBody>
-          </CCard>
-          <CCard
-            color="muted"
-            text-color="dark"
-            class="text-center py-5 d-sm-down-none"
-            body-wrapper
-          >
-            
-            <img v-bind:src="logo_path" width="80%"/>
-            <p>Welcome to Reflex, your Security Orchestration, Automation and Response Platform.  To get started, log in to the left. </p>
-            
-          </CCard>
-        </CCardGroup>
-      </CCol>
-    </CRow>
-  </CContainer>
+  <div class="flex-grid login-wrapper" style="margin-top: 0px">
+    <div class="d-col-3 login-box">
+      <CForm @submit.prevent="login">
+        <img v-bind:src="logo_path" class="login-logo"/>
+        <CInput
+          placeholder="Email"
+          autocomplete="username email"
+          required
+          v-model="email"
+        >
+          <template #prepend-content><CIcon name="cil-user"/></template>
+        </CInput>
+        <CInput
+          placeholder="Password"
+          type="password"
+          autocomplete="current-password"
+          required
+          v-model="password"
+        >
+          <template #prepend-content><CIcon name="cil-lock-locked"/></template>
+        </CInput>
+        <div class="flex-grid">
+          <div class="d-col">
+            <button class="btn btn-primary"  type="submit">Login</button>&nbsp;
+            <button class="btn btn-secondary" @click="loginWithSSO">Login with SSO</button>
+          </div>
+          <div class="d-col">
+            <button class="btn btn-secondary" to="/forgot_password">Forgot password?</button>
+          </div>
+        </div>
+      </CForm>
+    </div>
+    <div class="d-col-9 login-image">
+    </div>
+  </div>
 </template>
+
+<style scoped>
+
+/*
+.login-btn-primary {
+    background-color: #035657 !important;
+    border-color: #034444 !important ;
+    color: #fff
+}
+
+.login-btn-primary:hover {
+
+  background-color: rgba(0,0,0, 1.25)
+}
+
+.login-btn {
+  display: inline-block;
+  padding: 0.375rem 0.75rem;
+  font-weight: 400;
+  text-align: center;
+  vertical-align: middle;
+  line-height: 1.5;
+  border: 1px solid;
+  border-radius: 0.25rem;
+  background-color: #cfcfcf;
+  border-color: #9f9f9f;
+}
+
+.login-btn:focus {
+  outline: 2;
+  outline-color: #cfcfcf;
+}
+
+
+.login-btn:hover {
+
+  background-color: rgba(0,0,0, 0.25)
+}*/
+
+.login-logo {
+  width: 60%;
+  max-width: 300px;
+  margin-bottom: 10%;
+}
+
+.login-wrapper {
+  /* Should extend all the way to the bottom of the page  */
+  min-height: 100vh;
+  background-color: #333;
+  background-image: url('https://media.discordapp.net/attachments/1053061816453976124/1187490984288862208/netsurge_a_cyber_security_center_full_of_desks_and_monitors_the_6ffdc80f-4046-49fa-a7c1-d8e30288efd8.webp?ex=65971429&is=65849f29&hm=a27fc1d0dda5db64700a1466ff8b365210e40a833adb1b42bf561e6f06213f4b&=&format=webp&width=1920&height=1080');
+
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.login-box {
+  background-color: #ebedef;
+  padding: 25px !important;
+  justify-content: left;
+  min-height: 100vh;
+  /* Right shadow */
+  -webkit-box-shadow: 0px 0px 25px 0px rgba(0,0,0,0.75);
+  -moz-box-shadow: 0px 0px 25px 0px rgba(0,0,0,0.75);
+  box-shadow: 0px 0px 25px 0px rgba(0,0,0,0.75);
+}
+</style>
 
 <script>
 
